f679e7fda19eb3fd8ba87db1f3fb4af7526623d0
Firstname Lastname <101855974+Avishkasj@users.noreply.github.com>
2023-09-13 12:39:56 +0530

    add customer order data file

---
+++ lib/Customer/CustomerOrderData.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CustomerOrderData extends StatefulWidget {
+  const CustomerOrderData({Key? key}) : super(key: key);
+
+  @override
+  State<CustomerOrderData> createState() => _CustomerOrderDataState();
+}
+
+class _CustomerOrderDataState extends State<CustomerOrderData> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
